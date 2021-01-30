d75a7583cf1c735fa3911b7dfd0c0aff19871133
rob <robert.szczepanski@cmsrs.pl>
2021-01-30 12:40:33 +0100

    create tables

---
+++ database/migrations/2021_01_30_112408_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id')->index();
+            $table->string('name')->notNullable();
+            $table->integer('points')->nullable();
+            $table->unsignedBigInteger('project_id')->notNullable();
+            $table->foreign('project_id')->notNullable()->references('id')->on('projects')->onDelete('cascade');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
